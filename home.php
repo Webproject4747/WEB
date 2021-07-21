e0ca6bccec35eefa55ee73f5a6037633fcd3a16e
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-21 16:57:21 +0530

    Update home.php

--- home.php
+++ home.php
@@ -9,11 +9,16 @@
 <header class="header">
     <nav class= "flex items-center justify-between">
     <div class="left flex items-center">
-
-        <img src="images/vidya-vikas-logo.png"alt="logo">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#"></a>
+	<?php
+		session_start();
+		$connection = mysqli_connect("localhost","root","");
+		$db = mysqli_select_db($connection,"sms");
+	?>
+        <img src="user_images/vidya-vikas-logo.png"alt="logo">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#"></a>
         <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
         <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
         <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+		<center>Student &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email: <?php echo $_SESSION['email'];?>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Name:<?php echo $_SESSION['name'];?> 
         &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">about</a>
         &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="admin_dashboard.php">Admin</a>
         &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Contact</a>
@@ -26,39 +31,39 @@
 		
 		<div class="container"> 
 			<h1 class ="section-heading">Home</h1>
-				<p>Welcome to student management system </p>
+				<p>Welcome to Student Portal </p>
 				<div class="card-wrapper">
 					<div class="card">
-						<img src="images/student.png" alt="">
+						<img src="user_images/student.png" alt="">
 						
                         <a href="index.php"><h3>View personal detail</h3></a>
 					</div>
 					<div class="card">
-						<img src="images/acadamic.png" alt="">
+						<img src="user_mages/acadamic.png" alt="">
 						<h3>View acadamic detail</h3>
 					</div>
 					<div class="card">
-						<img src="images/feed.jpg" alt="">
+						<img src="user_images/feed.jpg" alt="">
 						<h3>Feedback</h3>
 					</div>
 					<div class="card">
-						<img src="images/notes.jpg" alt="">
+						<img src="user_images/notes.jpg" alt="">
 						<h3>Resources</h3>
 					</div>
                     <div class="card">
-						<img src="images/attendance.jpg" alt="">
+						<img src="user_images/attendance.jpg" alt="">
 						<h3>Attendance status</h3>
 					</div>
                     <div class="card">
-						<img src="images/certificate.jpg" alt="">
+						<img src="user_images/certificate.jpg" alt="">
 						<h3>Certificate</h3>
 					</div>
                     <div class="card">
-						<img src="images/book.png" alt="">
+						<img src="user_images/book.png" alt="">
 						<h3>Library</h3>
 					</div>
                     <div class="card">
-						<img src="images/apli.png" alt="">
+						<img src="user_images/apli.png" alt="">
 						<h3>Exam application</h3>
 					</div>
 				</div>
