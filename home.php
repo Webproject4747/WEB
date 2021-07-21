d65e5eea17be2df6705abd04f859d184c5de8d79
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-21 14:54:19 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="css/global.css" type="text/css" rel="stylesheet">
+</head>
+<body class="back">
+<header class="header">
+    <nav class= "flex items-center justify-between">
+    <div class="left flex items-center">
+
+        <img src="images/vidya-vikas-logo.png"alt="logo">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#"></a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">about</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="admin_dashboard.php">Admin</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Contact</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Logout</a>
+        
+</nav>
+    </header>   
+    <hr>
+<section class= "services">
+		
+		<div class="container"> 
+			<h1 class ="section-heading">Home</h1>
+				<p>Welcome to student management system </p>
+				<div class="card-wrapper">
+					<div class="card">
+						<img src="images/student.png" alt="">
+						
+                        <a href="index.php"><h3>View personal detail</h3></a>
+					</div>
+					<div class="card">
+						<img src="images/care-emoji-with-video-game.png" alt="">
+						<h3>View acadamic detail</h3>
+					</div>
+					<div class="card">
+						<img src="images/" alt="">
+						<h3>Feedback</h3>
+					</div>
+					<div class="card">
+						<img src="images/care-for-study.png" alt="">
+						<h3>Resources</h3>
+					</div>
+                    <div class="card">
+						<img src="images/care-for-study.png" alt="">
+						<h3>Attendance status</h3>
+					</div>
+                    <div class="card">
+						<img src="images/care-for-study.png" alt="">
+						<h3>Certificate</h3>
+					</div>
+                    <div class="card">
+						<img src="images/care-for-study.png" alt="">
+						<h3>Library</h3>
+					</div>
+                    <div class="card">
+						<img src="images/care-for-study.png" alt="">
+						<h3>Exam application</h3>
+					</div>
+				</div>
+		</div>
+		<br>
+		<br>
+		<br>
+		<br>
+	</section>
+</body>
+</html>
