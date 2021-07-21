e36b4ee863fffec5e8d15dc79f235c970c84d5af
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-21 10:38:47 -0700

    Add files via upload

---
+++ frontpage.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Main page</title>
+        <link rel="stylesheet" type="text/css" href="maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="css/global.css" type="text/css" rel="stylesheet">
+</head>
+<body class="back">
+
+<header class="header">
+    <nav class= "flex items-center justify-between">
+    <div class="left flex items-center">
+
+        <img src="images/vidya-vikas-logo.png"alt="logo">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a><a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a href="#">About</a>
+       
+</div>     
+</nav>
+    </header> 
+ 
+    <hr>
+<div>
+    <center><a><h2>Student management system</h2></a></center>
+</div>
+<div class="text">
+<br><br><br>
+<section class="services">
+    <div class="container">
+
+         <div class="card-wrapper">
+					<div class="card1">
+						<img src="" alt="">
+						<h3></h3>
+					</div>
+					<div class="card">
+						<img src="images/student.png" alt="">
+						<a href="index.php"><h3>Student</h3></a></div>
+                        <div class="card">
+						<img src="images/admin.png" alt="">
+						<a href="admin_log.php"><h3>Admin</h3>
+</div>
+</div>
+</div>
+                        
+</section>
+
+</body>
+</html>
