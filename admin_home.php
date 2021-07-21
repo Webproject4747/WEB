c8c1db17d055b7ac66d8bed61d0992957d7b6306
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-21 11:35:12 -0700

    Add files via upload

---
+++ admin_home.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Home Page</title>
+        <link rel="stylesheet" type="text/css" href="maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="css/global.css" type="text/css" rel="stylesheet">
+</head>
+<body class="back">
+<header class="header">
+    <nav class= "flex items-center justify-between">
+    <div class="left flex items-center">
+
+        <img src="images/vidya-vikas-logo.png"alt="logo">&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#"></a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        <a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">about</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="admin_dashboard.php">Admin</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Contact</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Logout</a>
+        
+</nav>
+    </header>   
+    <hr>
+<section class= "services">
+		
+		<div class="container"> 
+			<h1 class ="section-heading">Home</h1>
+				<p>Welcome to student management system </p>
+				<div class="card-wrapper">
+					<div class="card">
+						<img src="images/admin.png" alt="">
+						
+                        <a href="index.php"><h3>Search</h3></a>
+					</div>
+					<div class="card">
+						<img src="images/admin.png" alt="">
+						<h3>Add</h3>
+					</div>
+					<div class="card">
+						<img src="images/admin.png" alt="">
+						<h3>Delete</h3>
+					</div>
+					<div class="card">
+						<img src="images/admin.png" alt="">
+						<h3>Edit</h3>
+					</div>
+                    
+				</div>
+		</div>
+		<br>
+		<br>
+		<br>
+		<br>
+	</section>
+</body>
+</html>
