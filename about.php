237e7fa86829c0035ad181c214b200a65a256207
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-22 22:51:44 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+        <title>Login Page</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="Width-device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="css/global.css" type="text/css" rel="stylesheet">
+
+        <script  src="https://ajax.googleapls.com/ajax/libs/jquery/3.2.1/jquery_latest.js"></script>
+        <script  src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <header class="header">
+    <nav class= "flex items-center justify-between">
+    <div class="left flex items-center">
+
+        <img src="images/vidya-vikas-logo.png"alt="logo"><a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a><a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a><a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;</a>&nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="home.php">home</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">student</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="admin_dashboard.php">Admin</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;<a href="#">Notification</a>
+        &nbsp; &nbsp;&nbsp;&nbsp; <a href="#">Logout</a></header><hr>
+</head>
+<body class="back">
+    
+    <center><h2>Student portal</h2>
+    <br><br><br>
+    <strong><p>The student portal can be used by students and admin, It comprises of admin and student options which leads to respective login pages. Student can view his personal,acadamic.Access notifications, resources ,attendance status, ia marks , library, give feedback , apply for exam and get certificates and soon.  Whereas admin can add, search, delete or edit student details.</p>
+</center>
+</body>
+</html>
