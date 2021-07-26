e55f1e2c1f87f96864a937573742b2f77114b998
Webproject4747 <87755644+Webproject4747@users.noreply.github.com>
2021-07-26 11:59:36 +0530

    Update personal.php

--- personal.php
+++ personal.php
@@ -9,13 +9,7 @@
 
         <script  src="https://ajax.googleapls.com/ajax/libs/jquery/3.2.1/jquery_latest.js"></script>
         <script  src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-        <?php
-		session_start();
-		$name = "";
-		$connection = mysqli_connect("localhost:3325","root","");
-		$db = mysqli_select_db($connection,"sms");
-	?>
-</head>
+        
 <body class="back">
     <header class="header">
     <nav class= "flex items-center justify-between">
@@ -30,50 +24,74 @@
 </nav>
     </header>
     <hr>
-    <div id="right_side"><br><br>
-		<div id="demo">
-            <h2>Personal details</h2>
-		<!-- #Code for search student---Start-->
-			<?php
-				
-					$query = "select * from students";
-					$query_run = mysqli_query($connection,$query);
-					while ($row = mysqli_fetch_assoc($query_run)) 
+	<div class="container-fluid bg">
+        <div class="row">
+            <div class="col-md-2 col-sm-2 col-xs-8"></div>
+            <div class="col-md-2 col-sm-2 col-xs-8">
+            <br><br><br><br><br>
+            <center>
+            <form action="" method="POST" class="form-container">
+                <div class="form-group">
+                    <label><h3><strong>Search</strong></h3></label><br>
+                    <form action="" method="post">
+			USN: <input type="text" name="usn" required><br><br>
+			<input type="submit" name="per_search" value="Search">
+		</form><br>
+ 
+  <br>
+<?php
+session_start();
+if(isset($_POST['per_search'])){
+$connection = mysqli_connect("localhost:3325","root","");
+$db = mysqli_select_db($connection,"sms");
+  $query = "select * from personal where usn = '$_POST[usn]'";
+				$query_run = mysqli_query($connection,$query);
+				while ($row = mysqli_fetch_assoc($query_run))  
+					
 					{
                     
 						?>
+						<center>
 						<table>
-                        <center>
-							<tr>
-								<td>
-									<b>Usn:</b>
-								</td> 
-								<td>
-									<input type="text" value="<?php echo $row['Usn']?>" disabled>
-								</td>
-							</tr>
+                        
 							<tr>
 								<td>
 									<b>Name:</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Name']?>" disabled>
+									<input type="text" value="<?php echo $row['name']?>" disabled>
 								</td>
 							</tr>
 							<tr>
 								<td>
-									<b>Father's Name:</b>
+									<b>Father</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Father_name']?>" disabled>
+									<input type="text" value="<?php echo $row['father_name']?>" disabled>
 								</td>
 							</tr>
 							<tr>
 								<td>
-									<b>Branch,sem:</b>
+									<b>Mother:</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Branch']?>" disabled>
+									<input type="text" value="<?php echo $row['mother_name']?>" disabled>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<b>Present address:</b>
+								</td> 
+								<td>
+									<input type="text" value="<?php echo $row['present_address']?>" disabled>
+								</td>
+							</tr>
+							<tr>
+								<td>
+									<b>Permanent address:</b>
+								</td> 
+								<td>
+									<input type="text" value="<?php echo $row['permanent_address']?>" disabled>
 								</td>
 							</tr>
 							<tr>
@@ -81,29 +99,38 @@
 									<b>Phone:</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Phone']?>" disabled>
+									<input type="text" value="<?php echo $row['phone']?>" disabled>
 								</td>
 							</tr>
 							<tr>
 								<td>
-									<b>Email:</b>
+									<b>Religion:</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Email']?>" disabled>
+									<input type="text" value="<?php echo $row['Religion'] ?>" disabled>
 								</td>
 							</tr>
 							<tr>
 								<td>
-									<b>Password:</b>
+									<b>Siblings:</b>
 								</td> 
 								<td>
-									<input type="text" value="<?php echo $row['Password'] ?>" disabled>
+									<input type="text" value="<?php echo $row['siblings'] ?>" disabled>
 								</td>
 							</tr>
+							
                         </center>	
 						</table>
+						<input type="submit" name="back" value="Back" >
 						<?php
-					
+						
+						if(isset($_POST['back'])){
+							header("location:home.php");
+						}
+						?>
+						
+						<?php
+					}
 				}
 			?>
             </body>
